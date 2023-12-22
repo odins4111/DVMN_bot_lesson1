631d8695cdbf95f2877aaa2f8835a4f6fecf7c8f
odins4111 <33553704+odins4111@users.noreply.github.com>
2023-12-22 17:52:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,15 +20,15 @@ def get_homework_status(token_tg, token_devman, chat_id):
         text_message = "У вас проверили работу - ({0}) \n\nПреподавателю все понравилось, можно приступать к следующему уроку!".format(
             homeworks_roster["new_attempts"][0]["lesson_title"]
         )
-        strat_telegram_bot(text_message, token_tg, chat_id)
+        start_telegram_bot(text_message, token_tg, chat_id)
     else:
         text_message = "У вас проверили работу -{0}\n\nК сожалению, в работе нашлись ошибки".format(
             homeworks_roster["new_attempts"][0]["lesson_title"]
         )
-        strat_telegram_bot(text_message, token_tg, chat_id)
+        start_telegram_bot(text_message, token_tg, chat_id)
 
 
-def strat_telegram_bot(text_message, token_tg, chat_id):
+def start_telegram_bot(text_message, token_tg, chat_id):
     bot = telegram.Bot(token=token_tg)
     bot.send_message(chat_id=chat_id, text=text_message)
 
