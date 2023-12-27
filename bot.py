c71799b5dbd2f0a7b7f62b4cd5bdf4af6a63f766
odins <odins41@yandex.ru>
2023-12-27 21:27:31 +0300

    Убрал лишнюю функцию

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import argparse
 
 
 def get_homework_status(token_tg, token_devman, chat_id):
+    bot = telegram.Bot(token=token_tg)
     timestamp = ""
     url = "https://dvmn.org/api/long_polling/"
     headers = {"Authorization": token_devman}
@@ -20,17 +21,12 @@ def get_homework_status(token_tg, token_devman, chat_id):
         text_message = "У вас проверили работу - ({0}) \n\nПреподавателю все понравилось, можно приступать к следующему уроку!".format(
             homeworks_roster["new_attempts"][0]["lesson_title"]
         )
-        start_telegram_bot(text_message, token_tg, chat_id)
+        bot.send_message(chat_id=chat_id, text=text_message)
     else:
         text_message = "У вас проверили работу -{0}\n\nК сожалению, в работе нашлись ошибки".format(
             homeworks_roster["new_attempts"][0]["lesson_title"]
         )
-        start_telegram_bot(text_message, token_tg, chat_id)
-
-
-def start_telegram_bot(text_message, token_tg, chat_id):
-    bot = telegram.Bot(token=token_tg)
-    bot.send_message(chat_id=chat_id, text=text_message)
+        bot.send_message(chat_id=chat_id, text=text_message)
 
 
 def main():
